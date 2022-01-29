a9a1f50538fd4099a34b8da6a2fbf7788e918ba1
Aaron <meghan.elcheson@mail.mcgill.ca>
2022-01-29 11:37:16 -0500

    test

---
+++ main.py
@@ -0,0 +1,12 @@
+def main_wrapper():
+    print(f"This is the start of our Python project. This funciton's name is {main_wrapper.__name__}")
+
+    #code here
+
+
+
+
+    print("This is the end of our Python project.")
+
+if __name__ == "__main__":
+    main_wrapper()
